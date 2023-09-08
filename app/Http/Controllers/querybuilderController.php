4d446cd3672c9a07c8ef15283871e059dd967012
anakha-2001 <137748107+anakha-2001@users.noreply.github.com>
2023-09-08 15:20:09 +0530

    first commit

---
+++ app/Http/Controllers/querybuilderController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\DB;
+
+class querybuilderController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $data=DB::table('stu_mrk')->where('age','>',20)->where('age','>',24)->get();
+        dd($data);
+            // [
+            // "name"=>"anitha",
+            // "age"=>24,
+            // "marks"=>70
+            // ],
+            // [
+            //     "name"=>"amrutha",
+            //     "age"=>25,
+            //     "marks"=>55
+
+            // ]
+            // ]);
+            
+    }
+}
